04dbbd723c180248b75ac3246310440121dc72a4
txhai <trinhxhai2000@gmail.com>
2021-05-10 18:06:47 +0700

    first commit

---
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/IAuthorRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+using System.Threading.Tasks;
+
+namespace Acme.BookStore.Authors
+{
+    public interface IAuthorRepository : IRepository<Author, Guid>
+    {
+        Task<Author> FindByNameAsync(string name);
+
+        Task<List<Author>> GetListAsync(
+            int skipCount,
+            int maxResultCount,
+            string sorting,
+            string filter = null
+        );
+    }
+}
